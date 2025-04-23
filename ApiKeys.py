ca82bfcebd9f8e51a7795c39c8aed48e229bd82e
Oleg <98162381+zxclwnq@users.noreply.github.com>
2025-04-23 22:33:14 +0300

    Delete ApiKeys.py

--- ApiKeys.py
+++
@@ -1,9 +0,0 @@
-class ApiKey:
-    def __init__(self, _token):
-        self._token = _token
-
-    @property
-    def token(self):
-        return self._token
-
-TgApiKey = ApiKey("7262257458:AAE1rE1qELroz1Iv59h_lBFktKE4qE88uys")
